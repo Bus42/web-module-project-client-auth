57c9aa85452d96d48c2b5b180bd33e6508357559
Greg <gbrewton1980@gmail.com>
2021-10-04 13:13:24 -0600

    resets errors on success, allows multiple errors

--- friends/src/actions/authActions.js
+++ friends/src/actions/authActions.js
@@ -39,9 +39,24 @@ export const login = (credentials) => (dispatch) => {
       dispatch(authSuccess(data.payload));
       window.location = ("/friendslist")
     })
-    .catch(({ message }) => {
-      dispatch(authError(message));
-    })
+    .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      dispatch(authError(error.response.data))
+      dispatch(authError(error.response.status))
+      dispatch(authError(error.response.headers))
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      dispatch(authError(error.request))
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      dispatch(authError('Error', error.message))
+    }
+    dispatch(authError(error.config))
+  });
 };
 
 export const logout = () => (dispatch) => {
