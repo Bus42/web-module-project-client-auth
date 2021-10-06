6798441b81dfe884970982a3170afeef0bb102fd
Greg <gbrewton1980@gmail.com>
2021-10-05 19:48:04 -0600

    Minor upgrades to axiosWithAuth

--- friends/src/helpers/axiosWithAuth.js
+++ friends/src/helpers/axiosWithAuth.js
@@ -1,3 +1,5 @@
+// consider refactoring to a custom hook
+
 import axios from "axios";
 import { BASE_URL } from "../data";
 
@@ -5,11 +7,19 @@ const AUTH_TOKEN = window.localStorage.getItem("token") || null;
 
 const instance = axios.create({
   baseURL: BASE_URL,
-  timeout: 2000,
-  headers: {
-    authorization: AUTH_TOKEN,
+  // timeout: 2000, // un-comment this line to change default timeout of 0 (no timeout)
+  onUploadProgress: (event) => { // can be used for custom loading components or with styling libraries
+    console.log("upload: ", event);
+  },
+  onDownloadProgress: (event) => {
+    console.log("download: ", event);
   },
 });
+
+instance.interceptors.request.use((config) => {
+  const configWithToken = {...config, headers: {authorization: AUTH_TOKEN} }
+  return configWithToken;
+})
 
 const axiosWithAuth = ({ method, endpoint, body = null }) => {
   switch (method) {
